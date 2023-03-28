0ebe104770d3946e6036918114b31f8d661acb85
j1008612 <j1008612@GX9CF72J.jda.corp.local>
2023-03-28 17:09:14 -0400

    Formfillup commit

---
+++ Myseleproj/src/selenPkg/FormFillUp.java
@@ -0,0 +1,40 @@
+package selenPkg;
+
+
+		import org.openqa.selenium.By;
+		import org.openqa.selenium.WebDriver;
+		import org.openqa.selenium.chrome.ChromeDriver;
+		import org.openqa.selenium.support.ui.Select;
+
+		public class FormFillUp {
+			
+		public static void main(String[] args) {
+			
+		  
+		    WebDriver browserObject;
+		  //1. setup the property of chromedriver to pass value to form input box through chrome web browser.
+		    System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+			
+		    // 2. Initialize Webdriver object through ChromeDriver class.
+		    browserObject = new ChromeDriver();
+		    // 3. Open form page of http://www.training.qaonlinetraining.com/testPage.php
+		    browserObject.get("http://www.training.qaonlinetraining.com/testPage.php");
+		    
+		    // sending text to the input box in the web form--------------
+		    // 4. Locate Name section using name locator and send values using sendkeys
+		    browserObject.findElement(By.name("name")).sendKeys("DemoName");
+		 
+		    // 5. Locate email section using name locator and send values using sendkeys
+		    browserObject.findElement(By.name("email")).sendKeys("DemoEmail@demo.com");
+		    
+		   // 6. Locate website section using name locator and send values using sendkeys
+		    browserObject.findElement(By.name("website")).sendKeys("www.demosite.com");
+		    
+		    // 7. Locate comment section using name locator and send values using sendkeys
+		    browserObject.findElement(By.name("comment")).sendKeys("demo comment");
+		    
+		   // 8. Locate the submit button and perform click
+		    browserObject.findElement(By.name("submit")).click();
+	}
+
+}
