3b3e578e2c28ce5a13b700eabe573adbd55377b0
j1008612 <j1008612@GX9CF72J.jda.corp.local>
2023-03-29 15:01:57 -0400

    Addition commit

---
+++ MyJavaProj/src/myJavaPkg/Addition.java
@@ -0,0 +1,14 @@
+package myJavaPkg;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =-100; 
+		int b =50;
+		
+		int total = a+b;
+		System.out.println("Total " + total);
+	}
+}
+
