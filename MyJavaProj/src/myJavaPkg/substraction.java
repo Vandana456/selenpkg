31c496b8a5132999843de4bd067562ba6bf4c4bf
j1008612 <j1008612@GX9CF72J.jda.corp.local>
2023-03-29 15:17:09 -0400

    substraction commit

---
+++ MyJavaProj/src/myJavaPkg/substraction.java
@@ -0,0 +1,16 @@
+package myJavaPkg;
+
+public class substraction {
+	public class Substraction {
+
+		public static void main(String[] args) {
+			// Intialize value to variable and substract the number to print in console window 
+			int a= 500;
+			int b=100;
+			int sub = a-b;
+			
+			System.out.println("Substraction "+sub);
+
+}
+}
+}
